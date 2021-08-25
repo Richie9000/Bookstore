5e8a2c0b9859dfd84a7682f9c71e1ef7d7a10bbd
Ricardo <rr155914@gmail.com>
2021-08-25 16:08:02 -0500

    Add components to add/remove books

--- src/components/books.js
+++ src/components/books.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import AddBooks from './AddBooks';
+import BookPiece from './bookPiece';
 
 const Books = () => (
 
   <div>
-    Not yet implemented
+    <div>
+      <h1>Books</h1>
+      <ul>
+        <BookPiece />
+      </ul>
+    </div>
+    <div>
+      <AddBooks />
+    </div>
   </div>
 );
 
